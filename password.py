e8a9bcfe844f14871896aaf799d106eb89542603
prow34591 <67172485+prow34591@users.noreply.github.com>
2020-06-21 04:11:21 +0800

    Update password.py

--- password.py
+++ password.py
@@ -1,13 +1,14 @@
 password = "a5566"
 i = 3
-while True :
+while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
 		break 
 	else:
-		i = i - 1
-		print('密碼錯誤! 還有', i, '次機會')
-		if i == 0:
-			print('請等待15分鐘')
-			break
+		print('密碼錯誤!')
+		if i > 0:
+			print(' 還有', i, '次機會')
+		else:
+			print('請等待15分鐘! 在嘗試')
